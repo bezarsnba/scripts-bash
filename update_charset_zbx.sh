f504597cf2c8d40c5a496c1792944ab5659f870e
Beza <bezarsnba@gmail.com>
2020-04-14 10:11:37 -0300

    Add database update charset

--- update_charset_zbx.sh
+++ update_charset_zbx.sh
@@ -11,7 +11,7 @@ host=127.0.0.1
 
 querySQL='SELECT CONCAT("ALTER TABLE ", TABLE_NAME," CONVERT TO CHARACTER SET utf8 COLLATE utf8_bin;") AS alter_sql  FROM information_schema.`COLUMNS`  WHERE table_schema = "zabbix" AND CHARACTER_SET_NAME = "latin1";'
 queryValidate='SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, CHARACTER_SET_NAME, COLLATION_NAME  FROM information_schema.`COLUMNS` WHERE table_schema = "zabbix" AND COLLATION_NAME != "utf8";'
-
+queryUpdateDataBase='ALTER DATABASE `zabbix` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;'
 
 outputRes=$PWD/query_output.sql
 
@@ -22,7 +22,10 @@ sed -i.bkp '/alter_sql/d' $outputRes
 
 #cat $outputRes
 
+#Uptade Table Chareset
 mysql -p$pass -u$user -h$host --database $databaseName  -e "source $outputRes"
 
+#Update Database Charset
+mysql -p$pass -u$user -h$host --database $databaseName  -e "$queryUpdateDataBase"
 echo "Validando"
 mysql -p$pass -uroot -h$host --database  $databaseName -e "$queryValidate"
